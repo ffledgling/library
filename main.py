a5cf329fead69767a65f4d1cbf5e2ed4a213d679
Anhad Jai Singh <ffledgling@gmail.com>
2015-03-11 09:38:11 +0530

    New Approach added; refactoring; cleanup
    - Added new approach that uses Pairwise Label selection to build an SVM
    incrementally.
    - Refactored old code to be more modular and broke down old 'globby' code
    into more logically re-usable functions
    - Added a code cleanup TODO
    - Added a rudimentary objective function
    - Added a unifying Metrics class to store and hold all metrics
    - Additional functions to process Metrics

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.cm as cm
 import numpy as np
 
 import itertools
+import collections
 import pprint
 
 class Class(object):
@@ -24,7 +25,67 @@ class Class(object):
             raise Exception
         self.samples += sample
 
+class Metrics(object):
 
+    def __init__(self, value=None, accuracy=None, balance=None, overlap=None, margin=None, partition=None):
+        self.accuracy = accuracy
+        self.balance = balance
+        self.overlap = overlap
+        self.margin = margin
+        self.partition = partition
+
+        self.value = objective_function(accuracy=accuracy, balance=balance, overlap=overlap, margin=margin)
+
+    def __repr__(self):
+        return ('(Value: %s' ' Accuracy: %s, Balance: %s, Overlap: %s, Margin: %s, Partition: %s)'
+                % (repr(self.value),  repr(self.accuracy),  repr(self.balance),  repr(self.overlap),  repr(self.margin),  repr(self.partition)))
+
+
+def objective_function(accuracy=None, balance=None, overlap=None, margin=None):
+    # Take all the different things we measure and create a single unifying function
+    # use the value of this function to decide if something a good choice
+    # Values returned should always be b/w 0 and 1.
+
+    value = 1.0
+
+    if accuracy:
+        value *= accuracy
+    if balance:
+        value *= balance
+    if overlap:
+        value *= overlap
+    if margin:
+        value *= margin
+
+    return value
+
+def get_statistics(results):
+    # Takes a list of 'Metrics' as input
+    # Outputs:
+    #   Best value
+    #   Maximum accuracy
+    #   Average Value
+    #   Average Accuracy
+
+    max_val=0
+    max_acc=0
+    sum_val=0
+    sum_acc=0
+    size = len(results)
+
+    for metric in results:
+        if metric.accuracy > max_acc:
+            max_acc = metric.accuracy
+        if metric.value > max_val:
+            max_val = metric.value
+
+        sum_val += metric.value
+        sum_acc += metric.accuracy
+
+    print 'Best Accuracy: %s' % max_acc
+    print 'Highest Value: %s' % max_val
+    print 'Average Accuracy: %s' % str(sum_acc/size)
+    print 'Average Value: %s' % str(sum_val/size)
 
 
 def create_set(label_list):
@@ -50,40 +111,16 @@ def class_partitions(class_labels):
     # The frozenset method is a little hacky for duplicate elimination, optimize later
     splits = set()
     count = 0
+
     for split in itertools.combinations(CLASS_LABELS, len(CLASS_LABELS)/2):
-        #print set(split), CLASS_LABELS - set(split)
         count += 1
         splits.add(frozenset((frozenset(split),frozenset(CLASS_LABELS - set(split)))))
 
-    #print len(splits)
-    #print count
-
     return splits
-        
 
 
-if __name__ == '__main__':
-
-    TRAINING_DATA_PATH='optdigits/optdigits.tra'
-    TESTING_DATA_PATH='optdigits/optdigits.tes'
-    CLASS_LABELS = set(range(0,10)) # Class labels are b/w 0..9 (inclusive)
-
-
-    accuracy_list = []
-
-    # create training set
-    train = create_set(CLASS_LABELS)
-    #pprint.pprint(train)
-    populate_dataset(train, TRAINING_DATA_PATH)
-    #pprint.pprint(train)
-
-    # create testing set
-    test = create_set(CLASS_LABELS)
-    #pprint.pprint(train)
-    populate_dataset(test, TESTING_DATA_PATH)
-    #pprint.pprint(train)
-
-    partition_list = class_partitions(CLASS_LABELS)
+def bruteforce_SVM(train, test, class_labels):
+    partition_list = class_partitions(class_labels)
 
     for count, partition in enumerate(partition_list):
         partition = tuple(partition)
@@ -111,11 +148,6 @@ if __name__ == '__main__':
             twoclass_test_data += test[label]
             twoclass_test_label += [1]*len(test[label]) # add labels for each of the data points to the label list
 
-        #print twoclass_train_data
-        #print twoclass_train_label
-
-        #print len(twoclass_train_data)
-        #print len(twoclass_train_label)
 
         # Train the SVM here
         clf = svm.LinearSVC()
@@ -131,3 +163,107 @@ if __name__ == '__main__':
     pprint.pprint(sorted_accuracy_list)
     print ''
     print 'Highest Accuracy was achieved with the following partition', sorted_accuracy_list[-1]
+
+def pairwise_SVM_A1(test, train, class_labels):
+    # Take pairwise classes
+    # Compute SVM for that class
+    # Then put each class in one of the two major classes
+    # Then recompute accuracy on training set.
+    # do so until all classes are done.
+    # Compute the accuracy on the training set, keep track of results, find best result
+
+    count = 0
+    results = []
+
+    # For every possible pair of classes
+    for initial_two in itertools.combinations(class_labels, 2):
+        #print '#%s' % count
+
+        #print 'Initial Two labels of choice are:', initial_two
+        remaining_labels = set(class_labels - set(initial_two))
+        #print 'remaining labels are: %s' % remaining_labels
+        mapping = {0: {initial_two[0]}, 1: {initial_two[1]}}
+        clf = svm.LinearSVC()
+        # Train classifier based on initial two classes.
+        # extract data points from the train object, generate labels on the fly
+        clf.fit(train[initial_two[0]] + train[initial_two[1]], [0]*len(train[initial_two[0]]) + [1]*len(train[initial_two[1]]))
+
+        for label in remaining_labels:
+            score = clf.score(train[label], [0]*len(train[label]))
+            # This is interesting to print, and observe actual values
+            #print 'scoring (similarity to %s:' % initial_two[0], label, score
+            if score > 0.5:
+                mapping[0].add(label)
+            else:
+                mapping[1].add(label)
+        #print 'Optimal paritioning is:', mapping
+
+        # Create a new SVC
+        clf = svm.LinearSVC()
+
+        # Formulate training set and labels
+        train_samples, train_labels = [], []
+        for label in mapping[0]:
+            train_samples += train[label]
+            train_labels += [0]*len(train[label])
+        for label in mapping[1]:
+            train_samples += train[label]
+            train_labels += [1]*len(train[label])
+
+        # Train the new SVC, based on decided split
+        clf.fit(train_samples, train_labels)
+
+        # Generate reverse mapping
+        reverse_mapping = {}
+        for binary_label, labels in mapping.items():
+            for label in labels:
+                reverse_mapping[label] = binary_label
+
+        # Test it
+        test_samples, test_labels = [], []
+        for label in class_labels:
+            test_samples += test[label]
+            test_labels += [reverse_mapping[label]]*len(test[label])
+
+        #print 'Testing the re-trained optimal partition, score:',
+        a, b = len(mapping[0]), len(mapping[1])
+        current_metric = Metrics(accuracy=clf.score(test_samples, test_labels),
+                                 balance=2.0*min(a, b)/(a+b),
+                                 partition=tuple(mapping.itervalues()))
+
+        print current_metric
+        results.append(current_metric)
+        count += 1
+
+    return results
+
+
+if __name__ == '__main__':
+
+    # Do dataset specific things here in main
+
+    TRAINING_DATA_PATH='optdigits/optdigits.tra'
+    TESTING_DATA_PATH='optdigits/optdigits.tes'
+    CLASS_LABELS = set(range(0,10)) # Class labels are b/w 0..9 (inclusive)
+
+
+    accuracy_list = []
+
+    # create training set
+    train = create_set(CLASS_LABELS)
+    #pprint.pprint(train)
+    populate_dataset(train, TRAINING_DATA_PATH)
+    #pprint.pprint(train)
+
+    # create testing set
+    test = create_set(CLASS_LABELS)
+    #pprint.pprint(train)
+    populate_dataset(test, TESTING_DATA_PATH)
+    #pprint.pprint(train)
+
+    #results = bruteforce_SVM(train, test, CLASS_LABELS)
+    results = pairwise_SVM_A1(train, test, CLASS_LABELS)
+
+    pprint.pprint(results)
+    get_statistics(results)
+
